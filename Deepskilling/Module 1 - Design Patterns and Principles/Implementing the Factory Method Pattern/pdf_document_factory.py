6717400a66b17a5728f122533269ac35480eadff
Sowkhya Batchu <sowkhyabatchu@gmail.com>
2026-07-07 23:25:36 +0530

    Initial Commit

--- Deepskilling/Module 1 - Design Patterns and Principles/Implementing the Factory Method Pattern/pdf_document_factory.py
+++
@@ -1,7 +0,0 @@
-from document_factory import DocumentFactory
-from pdf_document import PdfDocument
-
-class PdfDocumentFactory(DocumentFactory):
-
-    def create_document(self):
-        return PdfDocument()
