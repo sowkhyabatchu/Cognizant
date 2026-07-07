633746118563d02a5b303357ba6566b7d752a62c
Sowkhya Batchu <sowkhyabatchu@gmail.com>
2026-07-07 22:56:33 +0530

    Implement Singleton pattern in single.py

---
+++ Deepskilling/Module 1 - Design Patterns and Principles/Implementing Singleton Patterns/single.py
@@ -0,0 +1,14 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+obj1 = Singleton()
+obj2 = Singleton()
+
+print(obj1)
+print(obj2)
+print(obj1 is obj2)
